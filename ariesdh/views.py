9e8d888af825bd7c29a4081cef976b1d0adf2eec
praveen <praveenporeddy8@gmail.com>
2023-10-09 13:14:52 -0500

    changed content

--- ariesdh/views.py
+++ ariesdh/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from azure.storage.fileshare import ShareFileClient
 import csv
 # Create your views here.
 def todolist(request):
